732f30321f8719966b09729844969e84c6eff686
Felipe Rocha <felipemota.rocha@gmail.com>
2020-08-31 16:27:22 -0300

    fix: redux logger middleware now are only being used in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import logger from "redux-logger";
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 
-import rootReducer from "./root-reducer";
-import rootSaga from "./root-saga";
+import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [logger, sagaMiddleware];
+const middlewares = [sagaMiddleware];
+if (process.env.NODE_ENV === 'development') {
+	middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
